49d33c24c0b07588fad7871d9a12f46509d5cf29
Summer <letaa1687@gmail.com>
2019-04-26 12:36:57 +0300

    slick

---
+++ js/main.js
@@ -0,0 +1,29 @@
+'use strict';
+
+jQuery(document).ready(function($) {
+      $('.rio-promos').slick({
+        dots: false,
+        infinite: true,
+        speed: 500,
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 2000,
+        arrows: true,
+        responsive: [{
+          breakpoint: 600,
+          settings: {
+            slidesToShow: 2,
+            slidesToScroll: 1
+          }
+        },
+        {
+           breakpoint: 400,
+           settings: {
+              arrows: false,
+              slidesToShow: 1,
+              slidesToScroll: 1
+           }
+        }]
+    });
+});
